65991a4f3ed16cbc25c0497b6c9d0c6f76a58e02
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-01 16:51:39 +0530

    Add files via upload

---
+++ DOT_NET/CategoryOfHeight.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace CategoryOfHeight;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter height: ");
+        double height = double.Parse(Console.ReadLine()) * 2.54;
+
+        if (height > 182)
+        {
+            Console.WriteLine("Your height is above 6 feet!");
+        }
+        else if (height < 182 && height > 152)
+        {
+            Console.WriteLine("your height is between 5 and 6 feet!");
+        }
+        else
+        {
+            Console.WriteLine("You are small!");
+        }
+    }
+}
