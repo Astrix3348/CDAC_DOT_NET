fd868f4d949d1aa7772eef64578a65ffcb393d40
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-01 16:40:49 +0530

    Add files via upload

---
+++ DOT_NET/AreaOfCircle.cs
@@ -0,0 +1,12 @@
+﻿namespace AreaOfCircle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter the radius: ");
+        int radius = int.Parse(Console.ReadLine());
+        float area = (float)(3.14 * radius * radius);
+        Console.WriteLine("Area of the circle is: " + area);
+    }
+}
