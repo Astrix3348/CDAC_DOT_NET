8fb5a01e4ebe617e2b33a0c89fb506daf30b28f0
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-01 17:02:00 +0530

    Add files via upload

---
+++ DOT_NET/NearestThou.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Nearesthou;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter number: ");
+        int num = int.Parse(Console.ReadLine());
+        int Nearesthousand = (int)Math.Round(num / 1000.0) * 1000;
+
+        Console.WriteLine("nearest thousand is: " + Nearesthousand);
+    }
+}
