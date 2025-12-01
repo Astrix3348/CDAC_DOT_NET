946e3e5f981a14d30b0d44fb7f8fd62f2ceb6ea1
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-01 15:58:17 +0530

    Add files via upload

---
+++ DOT_NET/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine("number entered is: " + x);
+    }
+}
