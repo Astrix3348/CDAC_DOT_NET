da3942f6de12f242000c210c615c98b3d7015dff
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-02 15:55:59 +0530

    Add files via upload

---
+++ DOT_NET/TypeConversion.cs
@@ -0,0 +1,15 @@
+﻿namespace TypeConversion;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number");
+        int x = int.Parse(Console.ReadLine());
+        int y = Convert.ToInt32(x);
+        double z = Convert.ToDouble(y);
+        string z2 = Convert.ToString(y);
+        Console.WriteLine(z2);
+    }
+}
+
