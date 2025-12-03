2732e3d99e93293a01892d2ff1e498e8e744db9f
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-03 10:26:16 +0530

    Add files via upload

---
+++ DOT_NET/DAY-2/FunctionOverloading.cs
@@ -0,0 +1,44 @@
+﻿namespace FunctionOverloading;
+
+public class TestingOverload
+{
+    public void add(int x, int y)
+    {
+        Console.WriteLine((x + y));
+    }
+
+    public void add(int x, int y, int z)
+    {
+        Console.WriteLine(x + y + z);
+    }
+
+    public void add(string x, string y)
+    {
+        Console.WriteLine("Concatenation of the two strings is: " + x + y);
+    }
+
+    public void add(double x, double y)
+    {
+        double z = x + y;
+        Console.WriteLine("Adding two Decimals: " + z.ToString());
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TestingOverload to = new TestingOverload();
+        to.add(4, 5);
+        to.add(5, 6, 7);
+        to.add(4.5, 7.7);
+        to.add("hello ", "world");
+
+        int x = Convert.ToInt32(Console.ReadLine());
+        double z = (double)x;
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        to.add(z, y);
+    }
+}
+
