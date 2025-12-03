30905408fb623864fdc50453741f8f3f101f0724
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-03 09:50:55 +0530

    Add files via upload

---
+++ DOT_NET/DAY-2/AbstractClasses.cs
@@ -0,0 +1,43 @@
+﻿namespace AbstractClasses;
+
+public abstract class CDAC
+{
+    public CDAC()
+    {
+        Console.WriteLine("Welcome to CDAC");
+        Console.ReadKey();
+    }
+    public void Courses()
+    {
+        Console.WriteLine("i have taken the course of DAC");
+        Console.ReadKey();
+    }
+}
+
+public class Student : CDAC
+{
+    public void study()
+    {
+        Console.WriteLine("i am currently working on a project");
+        Console.ReadKey();
+    }
+    public void address()
+    {
+        Console.WriteLine("i live in Dwarka, New Delhi!");
+        Console.ReadKey();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //CDAC c = new CDAC(); // cannot create an instance of an abstract class
+        Student st = new Student();
+        st.Courses();
+        st.study();
+        st.address();
+        
+    }
+}
+
