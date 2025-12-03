978f31a4dfb66ca00f0254b71129f4ce7c7bdaec
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-03 17:16:48 +0530

    Add files via upload

---
+++ DOT_NET/DAY-3/LargeNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+namespace LargeNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> ls = new List<int>();
+        Console.Write("How many numbers you want to add?: ");
+        int n = int.Parse(Console.ReadLine());
+        int max = 0;
+
+        while(n > 0)
+        {
+            Console.Write("Enter number: ");
+            int num = int.Parse(Console.ReadLine());
+            if(num > max)
+            {
+                max = num;
+            }
+            ls.Add(num);
+
+            n--;
+        }
+        Console.WriteLine("The maximum number is: " + max);
+    }
+    
+}
+
