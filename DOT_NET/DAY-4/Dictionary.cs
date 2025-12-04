5a5cc704aa875a48fbfc9423031fdbc40ee60d28
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-04 16:16:36 +0530

    Add files via upload

---
+++ DOT_NET/DAY-4/Dictionary.cs
@@ -0,0 +1,35 @@
+﻿namespace Dictionary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, string> employees = new Dictionary<int, string>() { { 1, "aakash" }, { 2, "aastha" } };
+
+        foreach(KeyValuePair<int, string> check in employees)
+        {
+            Console.Write("\nEmpId is: " + check.Key);
+            Console.Write(" EmpName is: " + check.Value);
+        }
+        Console.WriteLine();
+
+        //For displaying only keys
+        Dictionary<int, string>.KeyCollection keys = employees.Keys;
+
+        Console.WriteLine("===== Keys =====");
+        foreach(int item in keys)
+        {
+            Console.Write("\n" + item);
+        }
+
+        //For Values
+        Dictionary<int, string>.ValueCollection values = employees.Values;
+
+        Console.WriteLine("\n===== Values =====");
+        foreach(string value in values)
+        {
+            Console.Write("\n" + value);
+        }
+    }
+}
+
