245764c77d5fd1af078df126e0cb68734cc7c21f
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-04 15:07:02 +0530

    Add files via upload

---
+++ DOT_NET/DAY-4/SystemClassArray.cs
@@ -0,0 +1,26 @@
+﻿namespace SystemClassArray;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] arr = { 1, 2, 3, 4, 5, 6 };
+
+        //searching for an element
+        int n = Array.IndexOf(arr, 3);
+        Console.WriteLine(n);
+
+        //resizing of the array
+        int[] bArr = { 4, 6, 1 };
+        Array.Sort(bArr);
+
+        Array.Resize(ref bArr, 9);
+        Console.WriteLine(bArr.Count());
+        Console.WriteLine();
+        foreach(int x in bArr)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
+
