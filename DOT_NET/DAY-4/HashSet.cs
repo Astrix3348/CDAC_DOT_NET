5c8a04b1ecc1bac8ead91c274f5ad77744d7b41f
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-04 16:25:02 +0530

    Add files via upload

---
+++ DOT_NET/DAY-4/HashSet.cs
@@ -0,0 +1,22 @@
+﻿namespace HashSet;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<int> hs = new HashSet<int>();
+        hs.Add(42);
+        hs.Add(102);
+        hs.Add(22);
+        hs.Add(32);
+
+        hs.Remove(22);
+
+        Console.WriteLine("=====Displaying=====");
+        foreach (int i in hs)
+        {
+            Console.Write(i + "--");
+        }
+    }
+}
+
