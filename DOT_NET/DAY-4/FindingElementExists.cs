a79a9878de7c7c78bad659aadf51c26f33da35b1
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-04 15:34:09 +0530

    Add files via upload

---
+++ DOT_NET/DAY-4/FindingElementExists.cs
@@ -0,0 +1,26 @@
+﻿namespace FindingElement;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> marks = new List<int>() { 4, 5, 6, 2, 6, 9};
+
+        //checking if a student has failed or not
+        bool check = marks.Exists(m => m == 8);
+
+        if(check == true)
+        {
+            Console.WriteLine("Yes he has passed!");
+        }
+        else
+        {
+            Console.WriteLine("does not exist");
+        }
+
+        //finding the first failed marks
+        int firstFail = marks.Find(m => m < 5);
+        Console.WriteLine("First failed marks is: " + firstFail);
+    }
+}
+
