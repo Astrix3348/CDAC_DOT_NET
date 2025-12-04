b7d2c2a34d29e578f6f2a9fe55cd56b72e1352b7
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-04 16:38:12 +0530

    Add files via upload

---
+++ DOT_NET/DAY-4/SortedList.cs
@@ -0,0 +1,25 @@
+﻿namespace SortedList;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SortedList<int, string> sl = new SortedList<int, string>()
+        {
+            {5, "scott" },
+            {20, "allen" },
+            {3, "james" }
+        };
+        //adding element
+        sl.Add(6, "aakash");
+
+        //removing
+        sl.Remove(5);
+
+        foreach(var items in sl)
+        {
+            Console.WriteLine(items);
+        }
+    }
+}
+
