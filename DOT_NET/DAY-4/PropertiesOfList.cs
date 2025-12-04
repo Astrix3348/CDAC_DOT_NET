eeff23d7095be49f5394f06b7d117d3c068b8184
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-04 16:05:14 +0530

    Add files via upload

---
+++ DOT_NET/DAY-4/PropertiesOfList.cs
@@ -0,0 +1,26 @@
+﻿namespace PropertiesOfList;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> ls = new List<int>();
+
+        //adding new elements
+        ls.Add(1);
+        ls.Add(2);
+        ls.Add(3);
+
+        ls.Insert(1, 50);
+
+        List<int> extra = new List<int>() { 8, 9, 6, 1 };
+
+        ls.AddRange(extra);
+
+        foreach (int e in ls)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
+
