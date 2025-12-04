ad1e33c5de256ec1073889811e5c25bbc4eee588
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-04 16:32:40 +0530

    Add files via upload

---
+++ DOT_NET/DAY-4/HashTable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+namespace HashTableEx;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //creating a hashtable
+        Hashtable employees = new Hashtable()
+        {
+            {101, "scott" },
+            {102, "allen" },
+            {103, "james" }
+        };
+
+        
+        employees.Add(108, "aakash");
+        employees.Remove(101);
+
+        foreach(var item in employees)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
