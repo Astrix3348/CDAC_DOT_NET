9f90082cd3ebfa6ca824ae29f3eedd3cf9652837
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-05 10:45:33 +0530

    Add files via upload

---
+++ DOT_NET/DAY-5/DelegateExample.cs
@@ -0,0 +1,31 @@
+﻿namespace DelegateExample;
+
+
+//example of a multicast delegate
+class Program
+{
+    //create a delegate type
+    public delegate void Test(int x, int y);
+
+    static void Main(string[] args)
+    {
+        //create a delegate
+        Test obj1 = new Test(Addnumbers);
+        obj1 += new Test(Multiply);
+
+        obj1(10, 4);
+    }
+    //create a target method
+    static void Addnumbers(int x, int y)
+    {
+        Console.WriteLine("Addition is: " + (x + y));
+
+    }
+
+    //create another target method
+    static void Multiply(int x, int y)
+    {
+        Console.WriteLine("Multiplication is: " + (x * y));
+    }
+}
+
