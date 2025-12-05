5eb5559bc544724472a39f7eeb3ab941996e4303
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-05 10:37:09 +0530

    Add files via upload

---
+++ DOT_NET/DAY-5/AnonymousMethod.cs
@@ -0,0 +1,19 @@
+﻿namespace AnonymousMethod;
+
+public delegate string GetMessage(string name);
+class Program
+{
+    //anonymous methods are created using the delegate keyword
+    static void Main(string[] args)
+    {
+        //lambda expression
+        GetMessage gm = (name) =>
+        {
+            return "Hello " + name + " Welcome to Delhi!";
+        };
+
+        string Message = gm.Invoke("aakash");
+        Console.WriteLine(Message);
+    }
+}
+
