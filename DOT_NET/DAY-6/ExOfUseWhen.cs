1a11beeeef63e6f17c6fb3c17bb617295b0757b4
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-18 10:51:14 +0530

    Add files via upload

---
+++ DOT_NET/DAY-6/ExOfUseWhen.cs
@@ -0,0 +1,26 @@
+﻿namespace ExOfUseWhen;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        //app.MapGet("/", () => "Hello World!");
+
+        app.UseWhen(context => context.Request.Query.ContainsKey("username"),
+            app =>
+            {
+                app.Use(async (context, next) =>
+                {
+                    await context.Response.WriteAsync("hello from the UseWHen exmaple!");
+                    await next();
+                });
+            });
+        
+
+        app.Run();
+    }
+}
+
