6fe65bf6ec9f75259c86d054b8bafa3b709313e0
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-18 11:00:13 +0530

    Add files via upload

---
+++ DOT_NET/DAY-6/Assignment-1.1.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment_1._1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        //app.MapGet("/", () => "Hello World!");
+
+        app.Use(async (HttpContext context, RequestDelegate next) =>
+        {
+            string fulluser =  context.Request.Query["username"] + " " + context.Request.Query["password"];
+            await context.Response.WriteAsync(fulluser);
+            await next(context);
+        });
+
+        app.Run();
+    }
+}
+
