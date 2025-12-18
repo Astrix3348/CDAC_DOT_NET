469f6ecf4a9351935e3c324321ee87c78d49297b
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-18 16:50:10 +0530

    Add files via upload

---
+++ DOT_NET/DAY-6/ActionResult/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ExOfActionResult.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ExOfActionResult.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        public ContentResult Index()
+        {
+            return Content("<h1>Welcome to index</h1>");
+        }
+
+        [Route("about")]
+        public JsonResult About()
+        {
+            Person person = new Person()
+            {
+                id = Guid.NewGuid(),
+                firstName = "aakash",
+                lastName = "gangwar",
+                age = 22
+            };
+
+            return new JsonResult(person);
+        }
+
+        [Route("file-download")]
+        public VirtualFileResult fileDownLoad()
+        {
+            return new VirtualFileResult("/midreview.pdf", "application/pdf");
+        }
+
+        public PhysicalFileResult fileDown2()
+        {
+            return new PhysicalFileResult(@"Desktop/signalist_documentation.pdf", "application/pdf");
+        }
+    }
+}
+
---
+++ DOT_NET/DAY-6/ActionResult/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ExOfActionResult.Models
+{
+	public class Person
+	{
+		public Guid id { get; set; } //Guid is the globally unique id
+		public string? firstName { get; set; }
+		public string? lastName { get; set; }
+		public int age { get; set; }
+	}
+}
+
