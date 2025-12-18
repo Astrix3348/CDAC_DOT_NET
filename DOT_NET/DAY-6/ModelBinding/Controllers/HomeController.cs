d941c576777d9aa4e47358b2e193d1db22465dbe
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-18 17:43:10 +0530

    Add files via upload

---
+++ DOT_NET/DAY-6/ModelBinding/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ModelBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("bookstore/{bookid?}/{isLoggedin?}")] //routeData -- this is the highest priority
+        public IActionResult Index(int? bookid, bool? isLoggedin)
+        {
+            if(bookid.HasValue == false)
+            {
+                return BadRequest("book id is not given");
+            }
+            if(bookid <= 0)
+            {
+                return BadRequest("book id cannot be less than 0");
+            }
+
+            return new RedirectToActionResult("Books", "Store", new {}, true);
+        }
+    }
+}
+
---
+++ DOT_NET/DAY-6/ModelBinding/Controllers/StoreController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ModelBinding.Controllers
+{
+    public class StoreController : Controller
+    {
+        [Route("Store/Books")]
+        public IActionResult Books()
+        {
+            return Content("<h1>Book Store</h1>", "text/html");
+        }
+    }
+}
+
