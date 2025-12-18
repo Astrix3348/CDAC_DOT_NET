916fd944a259c29c9d5b71208c1025c6021b13dc
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-18 17:17:41 +0530

    Add files via upload

---
+++ DOT_NET/DAY-6/ForIActionResult/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ForIActionResult.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("bookstore")]
+        public IActionResult Index()
+        {
+            //book id should be provided
+            if (!Request.Query.ContainsKey("bookid"))
+            {
+                Response.StatusCode = 400; //in place of this
+                //we can also write this
+                return BadRequest("Book id is not provided");
+
+
+            }
+
+            //book id cannot be empty
+            if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
+            {
+                return BadRequest("Book id cannot be null or empty");
+            }
+
+            int bookid = Convert.ToInt32(ControllerContext.HttpContext.Request.Query["bookid"]);
+            if(bookid <= 0)
+            {
+                Response.StatusCode = 400;
+                return Content("book id cannot be less than 0");
+            }
+            if(bookid > 1000)
+            {
+                Response.StatusCode = 400;
+                return Content("book id cannot be greater than  999");
+            }
+            if (Convert.ToBoolean(Request.Query["isLoggedin"]) == false)
+            {
+                return Unauthorized("user must be authenticated");
+            }
+
+            return new RedirectToActionResult("Books", "Store", new { }, true);
+        }
+    }
+}
+
---
+++ DOT_NET/DAY-6/ForIActionResult/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ForIActionResult;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        var app = builder.Build();
+
+        app.MapControllers();
+        app.Run();
+    }
+}
+
