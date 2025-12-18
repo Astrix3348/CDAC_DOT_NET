61f3f6a95b5051364af33c1e8c59f0eeb5c0f3f2
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-18 15:57:42 +0530

    Add files via upload

---
+++ DOT_NET/DAY-6/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ExOfController.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        [Route("/")] //this is the default routing
+        public string Method1()
+        {
+            return "Hello This is me!";
+        }
+
+        [Route("/home")]
+        public string Home()
+        {
+            return "hello from home";
+        }
+    }
+}
+
