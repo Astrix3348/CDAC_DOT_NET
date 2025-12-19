877174b18c3c413a479b1bd312d4e83bb42b1b1d
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-19 17:13:33 +0530

    Add files via upload

---
+++ DOT_NET/Day-7/LayoutExample/LayoutExample/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LayoutExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
+
