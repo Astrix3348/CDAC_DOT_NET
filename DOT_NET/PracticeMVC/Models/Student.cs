41d08e847174ac39b4e50f44e6a4c5d46ed02e26
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2026-01-01 20:07:36 +0530

    Add files via upload

---
+++ DOT_NET/PracticeMVC/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PracticeMVC.Models
+{
+	public class Student
+	{
+		public int s_prn { get; set; }
+		public string s_name { get; set; }
+		public int s_fees { get; set; }
+
+	}
+}
+
